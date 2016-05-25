4ae5dcda610a1e190f0da63ceb5b76a209d2a356
Kyle Hopkins <kylehopkins1215@gmail.com>
2016-05-25 16:38:35 -0400

    Second chart and updated json data

--- javascript/roadClassCharts.js
+++ javascript/roadClassCharts.js
@@ -2,18 +2,16 @@
  * prep the data for populating the road data charts
  */
 function prepRoadDataForCharts() {
-  window.data = {labels: [], data: []};
+  window.data = {labels: []};
   window.sortable = [];
   var countries = getAllRoadData();
   Object.keys(countries).forEach(function (iso3) {
     var country = countries[iso3];
-
     sortable.push({label: country.name, country: country});
-
   });
 
   sortable.sort(function (a, b) {
-    return (a.country.records.total < b.country.records.total) ? 1 : -1;
+    return (a.country.classinfo.total < b.country.classinfo.total) ? 1 : -1;
   });
 }
 
@@ -21,10 +19,6 @@ function prepRoadDataForCharts() {
  * Populate the road data chart
 */
 function populateRoadDataChart() {
-    window.data.labels = sortable.map(function (item) {
-    return item.label;
-  });
-
   var sources = ['motorway', 'trunk', 'pmary', 'secondary', 'tertiary', 'unclassified', 'residential',  'serviceother'];
   var sources_colors = {
     'motorway':     '#ba1ab2',
@@ -37,9 +31,13 @@ function populateRoadDataChart() {
     'unclassified': '#aebcff'
   };
 
+	window.data.labels = sortable.map(function (item) {
+		return item.label;
+  });
+
   sources.forEach(function (source) {
     data[source] = window.sortable.map(function (item) {
-      return item.country.records[source] / item.country.records.total * 100;
+      return item.country.classinfo[source] / item.country.classinfo.total * 100;
     });
   });
   var datasets = [];
@@ -73,6 +71,64 @@ function populateRoadDataChart() {
     data: {
       labels: window.data.labels,
       datasets: datasets
+    }
+  });
+}
+
+function populateMaxSpeedChart() {
+	var sources = ['Motorway', 'Primary', 'Secondary', 'Trunk'];
+	var classSources = {
+					'Motorway': 'motorway',
+					'Primary': 'pmary',
+					'Secondary': 'secondary',
+					'Trunk': 'trunk'};
+  var sources_colors = {
+		'Motorway':  '#d4645c',
+    'Primary':   '#6ea0a4',
+    'Secondary': '#462272',
+    'Trunk':     '#d3c756'
+
+  };
+	window.data.labels = sortable.map(function (item) {
+		return item.label;
+	});
+	
+	sources.forEach(function (source) {
+    data[source] = window.sortable.map(function (item) {
+      return item.country.maxspeed[source] / item.country.classinfo[classSources[source]] * 25;
+    });
+  });
+	var datasets = [];
+  sources.forEach(function (source) {
+    datasets.push({
+      label: source,
+      data: window.data[source],
+      backgroundColor: sources_colors[source]
+    })
+  });
+
+	var ctx = document.getElementById("high-class-road-maxspeed").getContext('2d');
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: data.labels,
+      datasets: datasets
+    },
+    options: {
+      scales: {
+        xAxes: [{
+					stacked: true,
+          ticks: {
+            autoSkip: false
+          }
+        }],
+				yAxes: [{
+          stacked: true,
+          ticks: {
+            max: 100
+          }
+        }]
+      }
     }
   });
 }
